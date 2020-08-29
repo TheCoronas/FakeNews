390ba0c4ae185e06991aa2f1cae907fff7c4271f
jaidenharding <jaidenharding@gmail.com>
2020-08-29 18:42:26 +1000

    Basic Login Screen Implemented
    This commit contains a basic login screen, with default, hardcoded credentials. It also sets up a "Quit" button, which WILL NOT WORK UNTIL THE GAME IS BUILT. Signup has not yet been implemented.
    
    Moved some files around. All scenes are now located in "Assets>Scenes".

---
+++ FakeNewsProject/FakeNews/Assets/Scenes/Menus/LoginMenuControl.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoginMenuControl : MonoBehaviour
+{
+    public string username;
+    public string password;
+    public GameObject usernameInputField;
+    public GameObject passwordInputField;
+    public GameObject textDisplay;
+    
+    public void StoreName()
+    {
+        textDisplay.GetComponent<Text>().text = "Welcome " + username + " to the game.";
+    }
+    
+    public void ButtonLogin()
+    {
+        username = usernameInputField.GetComponent<Text>().text;
+        password = passwordInputField.GetComponent<Text>().text;
+//        textDisplay.GetComponent<Text>().text = "Incorrect Username or Password!";
+        if (username == "" && password == "") {
+            SceneManager.LoadScene(1);
+        } else {
+            textDisplay.GetComponent<Text>().text = "Incorrect Username or Password!";
+        }
+        
+    }
+    
+    public void ButtonReturn()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
---
+++ FakeNewsProject/FakeNews/Assets/Scenes/Menus/StartMenuControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenuControl : MonoBehaviour
+{
+    public void ButtonLogin()
+    {
+        SceneManager.LoadScene(2);
+    }
+    
+    public void ButtonSignUp()
+    {
+        SceneManager.LoadScene(3);
+    }
+    
+    public void ButtonQuit()
+    {
+        Application.Quit();
+    }
+}
