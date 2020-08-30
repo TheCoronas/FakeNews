d854f375d01e7d42b767909206d063f35545e401
jaidenharding <jaidenharding@gmail.com>
2020-08-30 15:16:04 +1000

    Added basic Map Screen and Pause Functionality
    Map screen will now display when "m" is pressed. Features a "Return to Game" button.
    
    A rough pause menu is implemented. Press "esc" to trigger. A few kinks to work out in terms of the buttons, but has the functionality to return to menu and resume game.

---
+++ FakeNewsProject/FakeNews/Assets/HUD/Scripts/MapScreen.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class MapScreen : MonoBehaviour
+{
+    public static bool mapDisplayed = false;
+    public GameObject mapMenu;
+    public GameObject player;
+    
+    void Update()
+    {
+        if (Input.GetKeyDown("m")) {
+            if (mapDisplayed == false)
+            {
+                Cursor.visible = true;
+                Time.timeScale = 0;
+                mapDisplayed = true;
+            
+                mapMenu.SetActive(true);
+                player.GetComponent<FirstPersonController>().enabled = false;
+            }
+            else
+            {
+                mapMenu.SetActive(false);
+                Cursor.visible = false;
+                mapDisplayed = false;
+                Time.timeScale = 1;
+                player.GetComponent<FirstPersonController>().enabled = true;
+            }
+            
+        }
+        
+        if (mapDisplayed == true) {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        } else {
+            Cursor.visible = false;
+        }
+    }
+    
+    public void returntoGame()
+    {
+        mapMenu.SetActive(false);
+        Cursor.visible = false;
+        mapDisplayed = false;
+        Time.timeScale = 1;
+        player.GetComponent<FirstPersonController>().enabled = true;
+    }
+}
---
+++ FakeNewsProject/FakeNews/Assets/Scenes/Menus/PauseGame.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+using UnityEngine.SceneManagement;
+
+public class PauseGame : MonoBehaviour
+{
+    public bool gamePaused = false;
+    public GameObject pauseMenu;
+    public GameObject player;
+    
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel")) {
+            if (gamePaused == false)
+            {
+                Time.timeScale = 0;
+                gamePaused = true;
+                Cursor.visible = true;
+                pauseMenu.SetActive(true);
+                player.GetComponent<FirstPersonController>().enabled = false;
+            }
+            else
+            {
+                pauseMenu.SetActive(false);
+                Cursor.visible = false;
+                gamePaused = false;
+                Time.timeScale = 1;
+                player.GetComponent<FirstPersonController>().enabled = true;
+            }
+        }
+        
+//        if (gamePaused == true ) {
+//            Cursor.visible = true;
+//            Cursor.lockState = CursorLockMode.None;
+//        } else {
+//            Cursor.visible = false;
+//        }
+    }
+            
+        
+    public void returntoGame()
+    {
+        pauseMenu.SetActive(false);
+        Cursor.visible = false;
+        gamePaused = false;
+        Time.timeScale = 1;
+        player.GetComponent<FirstPersonController>().enabled = true;
+    }
+    
+    public void returnToMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+        
+}
