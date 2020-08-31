41da25f098fc4fff7f7b7f237214c9be1fd0e374
ShadowDomo <44078427+ShadowDomo@users.noreply.github.com>
2020-08-31 15:39:26 +1000

    Update StartMenuControl.cs
    made cursor visible when you visit menu

--- FakeNewsProject/FakeNews/Assets/Scenes/Menus/StartMenuControl.cs
+++ FakeNewsProject/FakeNews/Assets/Scenes/Menus/StartMenuControl.cs
@@ -1,17 +1,25 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class StartMenuControl : MonoBehaviour
 {
+    public void Start()
+    {
+        Cursor.visible = true;
+    }
+
     public void ButtonLogin()
     {
+        // todo change to real scene name
         SceneManager.LoadScene(2);
     }
     
     public void ButtonSignUp()
     {
+        // todo change to real scene name
         SceneManager.LoadScene(3);
     }
     
