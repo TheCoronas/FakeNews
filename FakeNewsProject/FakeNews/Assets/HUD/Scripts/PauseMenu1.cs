92081dd23e8984ced47305a65a2e9e355d395902
sadjelena <jelena.ginger@gmail.com>
2020-08-31 10:05:47 +1000

    pause

---
+++ FakeNewsProject/FakeNews/Assets/HUD/Scripts/PauseMenu1.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu1 : MonoBehaviour
+{
+
+    [SerializeField] private GameObject pauseMenuUI;
+    [SerializeField] private bool isPaused; 
+
+    // Update is called once per frame
+    void Update()
+    { 
+        if(Input.GetKeyDown(KeyCode.P))
+        {
+            isPaused = !isPaused; 
+        }
+
+        if (isPaused)
+        {
+            ActivateMenu(); 
+        } else
+        {
+            DeactivateMenu(); 
+        }
+        
+    }
+
+    void ActivateMenu()
+    {
+        Time.timeScale = 0;
+        pauseMenuUI.SetActive(true);
+    }
+
+    void DeactivateMenu()
+    {
+        Time.timeScale = 1;
+        pauseMenuUI.SetActive(false); 
+    }    
+}
