9ea90a7f1de08d3582d13d86e54f8ae6ba87d0ff
ShadowDomo <44078427+ShadowDomo@users.noreply.github.com>
2020-08-31 16:11:31 +1000

    Update LoginMenuControl.cs
    can login now

--- FakeNewsProject/FakeNews/Assets/Scenes/Menus/LoginMenuControl.cs
+++ FakeNewsProject/FakeNews/Assets/Scenes/Menus/LoginMenuControl.cs
@@ -1,8 +1,14 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Net;
+using System.Text;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System.Security.Cryptography;
+using UnityEditor;
 
 public class LoginMenuControl : MonoBehaviour
 {
@@ -11,27 +17,61 @@ public class LoginMenuControl : MonoBehaviour
     public GameObject usernameInputField;
     public GameObject passwordInputField;
     public GameObject textDisplay;
-    
+
+
     public void StoreName()
     {
         textDisplay.GetComponent<Text>().text = "Welcome " + username + " to the game.";
     }
+
+   
     
     public void ButtonLogin()
     {
         username = usernameInputField.GetComponent<Text>().text;
         password = passwordInputField.GetComponent<Text>().text;
-//        textDisplay.GetComponent<Text>().text = "Incorrect Username or Password!";
-        if (username == "" && password == "") {
-            SceneManager.LoadScene(1);
-        } else {
+
+        // calculate hash
+        byte[] passBytes = Encoding.UTF8.GetBytes(password);
+        byte[] hashBytes = new SHA1Managed().ComputeHash(passBytes);
+        var sb = new StringBuilder(hashBytes.Length * 2);
+
+        // make hash readable
+        foreach (byte b in hashBytes) {
+            sb.Append(b.ToString("x2"));
+        }
+
+        string hash = sb.ToString();
+
+        var values = new NameValueCollection();
+        values["username"] = username;
+        values["password"] = hash;
+        
+        // temp credentials
+        // username: smith
+        // password: password
+       
+        // send to server
+        WebClient client = new WebClient();
+        byte[] response = client.UploadValues("https://corona.uqcloud.net/test/", values);
+        var result = Encoding.UTF8.GetString(response);
+    
+        Debug.Log(result);
+
+        if (result.Equals("false")) {
+            // todo change to real scene name
             textDisplay.GetComponent<Text>().text = "Incorrect Username or Password!";
+        } else if (result.Equals("")) {
+            textDisplay.GetComponent<Text>().text = "Please enter username and password!";
+        }else {
+            SceneManager.LoadScene(1);
         }
         
     }
     
     public void ButtonReturn()
     {
+        // todo change to real scene name
         SceneManager.LoadScene(0);
     }
 }
