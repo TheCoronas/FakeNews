0dc5331939c1802a403fb168f56b5675fe3a1540
sadjelena <jelena.ginger@gmail.com>
2020-09-10 23:23:11 +1000

    settings/sounds

---
+++ FakeNewsProject/FakeNews/Assets/SettingsMenu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PopupMessage : MonoBehaviour
+{
+
+    public GameObject ui;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void Open()
+    {
+
+         Time.timeScale = 0f;
+        
+    }
+    public void Close(){
+
+        Time.timeScale = 1f;
+    }
+
+}
