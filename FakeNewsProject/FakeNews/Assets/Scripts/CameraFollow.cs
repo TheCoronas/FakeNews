4b6a925bddf8e3d23ccb78af40873540fdfd5d42
sadjelena <jelena.ginger@gmail.com>
2020-09-13 12:26:56 +1000

    made a burning witch

---
+++ FakeNewsProject/FakeNews/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public GameObject _text;
+    public TextMesh _startText;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (_text == null) _text = GameObject.Find("StartText");
+        if (_startText == null) _startText = GameObject.Find("StartText").GetComponent<TextMesh>();
+        _text.SetActive(true);
+        _text.transform.Rotate(90f, 90f, 90f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print("Text position is: " + _text.transform.position); 
+        if (_text.activeSelf)
+        {
+            var camPos = Camera.main.transform.position + Camera.main.transform.forward;
+            _text.transform.position = camPos;
+            _text.transform.localScale = Vector3.one * 0.025f;
+        }
+
+        else
+        {
+            Debug.Log("deactive _startText");
+        }
+
+    }
+}
