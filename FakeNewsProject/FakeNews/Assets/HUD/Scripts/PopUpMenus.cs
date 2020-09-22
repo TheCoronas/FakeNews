1e5ecda825e65b7b515395b8cb8c8369a87883f8
maderama <madisonbeare@gmail.com>
2020-09-22 23:31:19 +1000

    Scroll Implementation Complete
    Can now view different scrolls from different characters

--- FakeNewsProject/FakeNews/Assets/HUD/Scripts/PopUpMenus.cs
+++ FakeNewsProject/FakeNews/Assets/HUD/Scripts/PopUpMenus.cs
@@ -19,7 +19,11 @@ public class PopUpMenus : MonoBehaviour
     public GameObject mapMenu;
     public GameObject player;
     public GameObject pauseMenu;
-    public GameObject scrollDisplay;
+    public GameObject scrollDisplay1;
+    public GameObject scrollDisplay2;
+    public GameObject scrollDisplay3;
+    public GameObject scrollDisplay4;
+    public GameObject scrollDisplay5;
     public GameObject gameOverMenu;
     public GameObject explanationView; 
     public GameObject incorrectCharacter; 
@@ -92,11 +96,27 @@ public class PopUpMenus : MonoBehaviour
     {
         Time.timeScale = Math.Abs(Time.timeScale - 1);
         showScroll = !showScroll;
-    
-        // currentScrollDisplay = "ScrollDisplay" + (SelectObject.currentCharacter);
-        // Debug.Log(currentScrollDisplay);
-        // scrollDisplay = currentScrollDisplay;
-        scrollDisplay.SetActive(!scrollDisplay.activeInHierarchy);
+
+        switch (characterCount) {
+            case 1: 
+                scrollDisplay1.SetActive(!scrollDisplay1.activeInHierarchy);
+                break;
+            case 2:
+                scrollDisplay2.SetActive(!scrollDisplay2.activeInHierarchy);
+                break;
+            case 3:
+                scrollDisplay3.SetActive(!scrollDisplay3.activeInHierarchy);
+                break;
+            case 4:
+                scrollDisplay4.SetActive(!scrollDisplay4.activeInHierarchy);
+                break;
+            case 5:
+                scrollDisplay5.SetActive(!scrollDisplay5.activeInHierarchy);
+                break;
+            default: 
+                Debug.Log("Error in PopUpMenus.toggleScroll()");
+                break; 
+        }
         player.GetComponent<FirstPersonController>().enabled = false;
     }
 
@@ -130,7 +150,12 @@ public class PopUpMenus : MonoBehaviour
     
     public void returnToGameFromScroll()
     {
-        scrollDisplay.SetActive(false);
+        scrollDisplay1.SetActive(false);
+        scrollDisplay2.SetActive(false);
+        scrollDisplay3.SetActive(false);
+        scrollDisplay4.SetActive(false);
+        scrollDisplay4.SetActive(false);
+
         correctExplanationView.SetActive(false); 
         incorrectExplanationView.SetActive(false); 
         incorrectCharacter.SetActive(false);
@@ -173,7 +198,12 @@ public class PopUpMenus : MonoBehaviour
 
     public void enterCorrectExplanation()
     {
-        scrollDisplay.SetActive(false);
+        scrollDisplay1.SetActive(false);
+        scrollDisplay2.SetActive(false);
+        scrollDisplay3.SetActive(false);
+        scrollDisplay4.SetActive(false);
+        scrollDisplay4.SetActive(false);
+
         displayCorrectExplanation = true;
         Time.timeScale = Math.Abs(Time.timeScale - 1);
 
@@ -184,7 +214,12 @@ public class PopUpMenus : MonoBehaviour
 
     public void enterIncorrectExplanation()
     {
-        scrollDisplay.SetActive(false);
+                scrollDisplay1.SetActive(false);
+        scrollDisplay2.SetActive(false);
+        scrollDisplay3.SetActive(false);
+        scrollDisplay4.SetActive(false);
+        scrollDisplay4.SetActive(false);
+
         displayIncorrectExplanation = true;
         Time.timeScale = Math.Abs(Time.timeScale - 1);
 
