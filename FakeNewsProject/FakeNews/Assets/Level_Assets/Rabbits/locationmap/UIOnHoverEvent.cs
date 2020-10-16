c9d10593a482b46de8daa7b1dfd89e3115c05c3c
sadjelena <jelena.ginger@gmail.com>
2020-10-16 22:50:37 +1000

    ooooooofffffffffffff screwed up something :( - problem for another day

---
+++ FakeNewsProject/FakeNews/Assets/Level_Assets/Rabbits/locationmap/UIOnHoverEvent.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine.SocialPlatforms.GameCenter;
+using UnityEngine.UI;
+
+public class UIOnHoverEvent : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+
+    Vector3 cachedScale;
+    private Text inftext;
+    private Vector3 textloc = new Vector3(1000f, 240f, 100f); 
+
+    void Start()
+    {
+        inftext = GameObject.Find("infoPhys").GetComponent<Text>();
+        cachedScale = transform.localScale;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        inftext.text = ""; 
+        transform.localScale = new Vector3(1.2f, 1.2f, 1.2f);
+        GameObject LandInfo = transform.Find("info").gameObject;
+        LandInfo.SetActive(true);
+        LandInfo.transform.position = textloc;  
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        inftext.text = "Hover to learn your geography"; 
+        transform.localScale = cachedScale;
+        transform.Find("info").gameObject.SetActive(false);
+        inftext.transform.position = textloc;
+
+    }
+}
