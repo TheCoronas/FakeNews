ac207d27619d49be8f1f580b33f8aa69e9131727
jaidenharding <jaidenharding@gmail.com>
2020-10-28 00:27:42 +1000

    Restructred file layout for submisison

---
+++ FakeNewsProject/FakeNews/Assets/Scenes/Menus/Menu Scripts/TutorialMenuControl.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TutorialMenuControl : MonoBehaviour
+{
+    public void Start()
+    {
+        Cursor.visible = true;
+    }
+
+    public void ButtonAbout()
+    {
+        // todo change to real scene name
+        SceneManager.LoadScene("Scenes/Menus/Menu Scenes/intro1Scene 1");
+    }
+
+    public void ButtonNext()
+    {
+        // todo change to real scene name
+        SceneManager.LoadScene("Scenes/Menus/Menu Scenes/intro2Scene");
+    }
+
+    public void ButtonReturn()
+    {
+        SceneManager.LoadScene("Scenes/Menus/Menu Scenes/start_screen");
+    }
+}
