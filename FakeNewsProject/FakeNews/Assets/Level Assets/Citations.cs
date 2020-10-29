394125b961a4e8fe8bd0ca2c40c327dcb41d95b0
tjwright1 <69612041+tjwright1@users.noreply.github.com>
2020-10-29 18:47:08 +1000

    Citations + Deleted useless folder

---
+++ FakeNewsProject/FakeNews/Assets/Level Assets/Citations.cs
@@ -0,0 +1,44 @@
+﻿/**
+ * CITATIONS
+ * @author Madison Beare
+ * HUD BAR & IMAGES (NON-COMMERCIAL USE APROVED)
+ * [1] Thirslund A. (2020) https://github.com/Brackeys/Health-Bar/blob/master/Health%20Bar/Assets/Sprites/Bar.png Accessed: 4 Aug 2020
+ * [2] HiClipArt (n.d.) Hearts Art,  red heart transparent background PNG clipart, (https://www.hiclipart.com/free-transparent-background-png-clipart-qowup) Accessed: 13 Aug 2020
+ * [3] DLPNG (n.d.) Coin-Background-Coins-Transparant (https://dlpng.com/png/185509) Accessed: 13 Aug 2020
+ * 
+ * INSPECT MENU
+ * [1] PinClipArt (n.d.) Red Wax Seal (https://www.pinclipart.com/pindetail/bwbboo_red-wax-seal-1png-transparent-image-circle-clipart/) Accessed: 13 Aug 2020
+ * 
+ * SCROLL DECORATIONS
+ * [1] ClipArt Panda (n.d.) http://www.clipartpanda.com/clipart_images/mud-clipart-68999825 (http://www.clipartpanda.com/clipart_images/mud-clipart-68999825) Accessed: 13 Aug 2020
+ * 
+ * ASSETS
+ * @author Jelena Price, Troy Wright
+ * [1] Koeing, M. (2009).Please Sound(http://soundbible.com/1277-Please.html) Accessed: 10 Sep 2020
+ * [2] Nakarada, A. (n.d.) We Three Celtic Kings (https://www.free-stock-music.com/alexander-nakarada-we-three-celtic-kings.html) Accessed: 6 Sep 2020
+ * [3] BG Studio (2020). 8K Skybox Pack Free (https://assetstore.unity.com/packages/2d/textures-materials/sky/8k-skybox-pack-free-150926 ) Accessed: 12 Sep 2020
+ * [4] Unity Technologies (2019). Terrain Tools Sample Asset Pack (https://assetstore.unity.com/packages/2d/textures-materials/nature/terrain-tools-sample-asset-pack-145808) Accessed: 20 Aug 2020
+ * [6] 3DForge (2020). Blacksmith's Forge (https://assetstore.unity.com/packages/3d/environments/fantasy/blacksmith-s-forge-17785). Accessed: 27 Aug 2020
+ * [5] AndreiCG (2016). Church 3D (https://assetstore.unity.com/packages/3d/environments/fantasy/church-3d-68143) Accessed: 19 Sep 2020
+ * [7] Lukaszuk S. (2017). Lowpoly medieval buildings (https://assetstore.unity.com/packages/3d/environments/historic/lowpoly-medieval-buildings-58289) Accessed: 12 Sep 2020
+ * [8] Ferocious Industries (2019) FREE Medieval Structure Kit (https://assetstore.unity.com/packages/3d/environments/fantasy/free-medieval-structure-kit-141700) Accessed: 3 Sep 2020
+ * [9] Flegmatik (2016) Exterior Modular Village  (https://assetstore.unity.com/packages/3d/environments/historic/exterior-modular-village-40461) Accessed: 7 Sep 2020
+ * [10] Niwashi Games (2019) White Rabbit (https://assetstore.unity.com/packages/3d/characters/animals/white-rabbit-138709) Accessed: 20 Aug 2020
+ * [11] shui861wy (2018) Rock package (https://assetstore.unity.com/packages/3d/props/exterior/rock-package-118182) Accessed: 30 Aug 2020
+ * [12] Ferocious Industries (2018) FREE Medieval Props Asset Pack (https://assetstore.unity.com/packages/3d/props/free-medieval-props-asset-pack-131420) Accessed: 21 Aug 2020
+ * [13] Max, A., Ultimate Fantasy Creator LITE (2015). https://assetstore.unity.com/packages/3d/environments/fantasy/ultimate-fantasy-creator-lite-45579#releases, Accessed 31 Aug 2020
+ * [14] Kawetofe, Medieval Tent Small (2017). https://assetstore.unity.com/packages/3d/environments/fantasy/medieval-tent-small-18736#description, Accessed 5 Sep 2020
+ * [15] Karboosx, Mega Fantasy Props Pack (2018). https://assetstore.unity.com/packages/3d/environments/fantasy/mega-fantasy-props-pack-87811, Accessed 10 Sep 2020
+ * [16] GBAndrewGB, VILLAGE HOUSES PACK (2018), https://assetstore.unity.com/packages/3d/characters/village-houses-pack-63695#content, Accessed 14 Sep 2020
+ * [17] Aquarius Max, Build-A-House Workshop (2019). https://assetstore.unity.com/packages/3d/environments/fantasy/build-a-house-workshop-63203#content, Accessed 10 Sep 2020
+ * [18] PinClipart, Red Wax Seal 1のpng Transparent Image - Circle Clipart (n.d.). https://www.pinclipart.com/pindetail/bwbboo_red-wax-seal-1png-transparent-image-circle-clipart/, Accessed 2 Oct 2020
+ * [19] Unity Technologies, Unity Particle Pack 5.x (2020). https://assetstore.unity.com/packages/essentials/asset-packs/unity-particle-pack-5-x-73777, Accessed 12 Sep 2020
+ * [20] Butterfly World, [BFW]Simple Dynamic Clouds (2017). https://assetstore.unity.com/packages/tools/particles-effects/bfw-simple-dynamic-clouds-85665, Accessed 20 Sep 2020
+ * [21] Lylek Games, Medieval Town Exteriors (2020). https://assetstore.unity.com/packages/3d/environments/fantasy/medieval-town-exteriors-27026, Accessed 4 Sep 2020
+ * [22] Dreamdev Studios, Campfire Pack (2013). https://assetstore.unity.com/packages/3d/environments/fantasy/campfire-pack-11256, Accessed 5 Sep 2020
+ * [23] John’s Junkyard Assets, PBR Ground Materials #1 [Dirt & Grass] (2017). https://assetstore.unity.com/packages/2d/textures-materials/floors/pbr-ground-materials-1-dirt-grass-85402, Accessed 31 Aug 2020
+ * [24] Kawetofe, Medieval Tent Big (2017), https://assetstore.unity.com/packages/3d/environments/historic/medieval-tent-big-19023, Accessed 5 Sep 2020
+ * [25] Morillas, W., Basic Bandit (2017), https://assetstore.unity.com/packages/3d/characters/humanoids/humans/basic-bandit-89978, Accessed 31 Aug 2020
+ * [26] Polytope Studio, Lowpoly Medieval Peasants - Free Pack, (2020). https://assetstore.unity.com/packages/3d/characters/humanoids/humans/lowpoly-medieval-peasants-free-pack-122225, Accessed 3 Sep 2020
+ * [27] ALP8310, Grass Flowers Pack Free (2019). https://assetstore.unity.com/packages/2d/textures-materials/grass-flowers-pack-free-138810, Accessed 30 Aug 2020
+ */
